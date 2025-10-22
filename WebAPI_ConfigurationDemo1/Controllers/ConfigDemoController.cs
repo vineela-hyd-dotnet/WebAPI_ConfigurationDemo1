5ce118c483b7dacd2fdb81137c1cae890ede8495
vineela-hyd-dotnet <vinnujella@gmail.com>
2025-10-22 17:52:03 +0530

    Add project files.

---
+++ WebAPI_ConfigurationDemo1/Controllers/ConfigDemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_ConfigurationDemo1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigDemoController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public ConfigDemoController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpGet]
+        public IActionResult GetAppSettings()
+        {
+            var appName = _config["AppSettings:ApplicationName"];
+            var version = _config["AppSettings:Version"];
+            return Ok(new {appName,version});
+        }
+
+    }
+}
